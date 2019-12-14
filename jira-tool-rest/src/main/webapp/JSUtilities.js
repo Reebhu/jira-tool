a3dac9bd4470d05314084e0c6c113426e7a50192
Pillai,Subbulakshmi <MSubbulakshmi.Pillai@cerner.com>
2019-12-15 01:43:20 +0530

    Home page and js utilities to handle request and response

--- jira-tool-rest/src/main/webapp/JSUtilities.js
+++ jira-tool-rest/src/main/webapp/JSUtilities.js
@@ -1,5 +1,6 @@
 var username;
 var password;
+var url;
 
 function login()
 {
@@ -10,7 +11,7 @@ function login()
 	requestJson.username = username;
 	requestJson.password = password;
 	
-	fetch('http://localhost:8080/jira-tool-rest/webapi/jira/user', 
+	fetch('http://localhost:8080/jira-tool-rest/webapi/jira/login', 
 		 {
 		  	method: 'POST',
 		  	headers: 
@@ -22,11 +23,111 @@ function login()
 		 .then(function(loginResponse)
 				 {
 			 		if(loginResponse.status === 200)
-			 			window.open("FilterIssues.html");
+			 		{
+			 			var loginBtn = document.getElementById("login");
+			 			loginBtn.parentNode.removeChild(loginBtn);
+			 			
+			 			var logoutBtn = document.createElement("input");
+			 			logoutBtn.type = "button";
+			 			logoutBtn.className = "btn btn-primary";
+			 			logoutBtn.value = "Sign Out";
+			 			logoutBtn.setAttribute("onclick", "logout()");
+			 			document.getElementById("login-out-toggle").appendChild(logoutBtn);
+			 			
+			 			document.getElementById("sidebar").style.display = "block";
+			 		}
 			 		else
 			 		{
 			 			alert("Could not initialize the user");
 			 		}
 			 	 }
 		 		);	
+}
+
+function getIssuesCurrentUser()
+{
+	alert("getIssuesCurrentUser");
+	url = "http://localhost:8080/jira-tool-rest/webapi/jira/issues";
+	fetch(url).then(function(response)
+			  {
+				  response.text().then(function(jsonResponse) 	
+				  {    
+					  alert(jsonResponse);
+					  var detailTable = document.getElementById("cardDetails");
+					  
+					  var issues = JSON.parse(jsonResponse);
+					  var data = '';
+					  
+					  for(var issue in issues)
+					  {
+						  data += "<tr>";
+						  	data += "<td> " + issue + " </td> <td>" + issues[issue] + "</td>";
+						  data += "</tr>"
+					  }
+					  
+					  detailTable.innerHTML = data;
+				  });
+			  }).catch(function(err) 
+			  {
+				    console.log('Fetch Error :-S', err);
+			  });	
+	alert("Dont wait");
+}
+
+function getWorklogCurrentUser()
+{
+	alert("getWorklogCurrentUser");
+	url = "http://localhost:8080/jira-tool-rest/webapi/jira/worklog";
+	fetch(url).then(function(response)
+			  {
+				  response.text().then(function(jsonResponse) 	
+				  {    
+					  alert(jsonResponse);
+					  var detailTable = document.getElementById("cardDetails");
+					  
+					  var worklogs = JSON.parse(jsonResponse);
+					  var data = '';
+					  
+					  alert(worklogs);
+					  
+					  
+					  detailTable.innerHTML = data;
+				  });
+			  }).catch(function(err) 
+			  {
+				    console.log('Fetch Error :-S', err);
+			  });	
+	alert("Dont wait");
+}
+
+function logout()
+{
+	username = document.getElementById('username').value;
+	password = document.getElementById('password').value;
+	
+	let requestJson = {};
+	requestJson.username = username;
+	requestJson.password = password;
+	
+	fetch("http://localhost:8080/jira-tool-rest/webapi/jira/logout", 
+			 {
+			  	method: 'DELETE',
+			  	headers: 
+			  	{
+			  		'Content-Type': 'application/json;charset=utf-8'
+			  	},
+			  	body: JSON.stringify(requestJson)
+			 })
+			 .then(function(logoutResponse)
+					 {
+				 		if(logoutResponse.status === 204)
+				 		{
+				 			window.open("http://localhost:8080/jira-tool-rest/");
+				 		}
+				 		else
+				 		{
+				 			
+				 		}
+				 	 }
+			 		);	
 }
--- jira-tool-rest/src/main/webapp/index.jsp
+++ jira-tool-rest/src/main/webapp/index.jsp
@@ -9,26 +9,45 @@
 </head>
 <body>
 	
-    	<div style="height:55px; background-color: Black;">
-    		<p align="right"><font size="6" color="white" face="verdana">&emsp;Jira Tool&emsp;</font></p>
+    	<div style="height:100px; background-color: Black;">
+    		<p align="left"><font size="6" color="white" face="verdana"><br/> &emsp; Jira Tool </font></p>
+    		
+    		<p align="right" style="margin-top: -90px">
+    		<table cellpadding="10" style="border:2px;">
+    			<tr>
+    				<td><p align="center"><font size="5" color="white" face="verdana"> Username </font></p>	</td>
+    				<td><p align="center"><font size="5" color="white" face="verdana"> Password </font></p>	</td>	
+    			</tr>	
+				<tr>
+					<td><input type="text" class="form-control" name="username" id="username" style="width: 220px;"></td>			
+					<td><input type="password" class="form-control" name="password" id="password" style="width: 220px;"></td>
+					<td id="login-out-toggle">
+						<button type="button" id="login" class="btn btn-primary" onclick="login()"> 
+							<font size="3" face="verdana"> Sign In </font>
+						</button>
+					</td>
+				</tr>
+			</table>
+			</p>
     	</div>
     	
-    	<div class="row" style="margin-top: 80px;">
-    		<div class="col-lg-5 col-md-5 col-sm-5 col-xs-2"></div>	
-			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-8 login-form">
-				<center>
-				<label><p align="center"><font size="5" color="black" face="verdana"> Username </font></p></label>				
-				<input type="text" class="form-control" name="username" id="username" style="margin-bottom:20px" align="center">
-				
-				<label><p align="center"><font size="5" color="black" face="verdana"> Password </font></p></label>				
-				<input type="password" class="form-control" name="password" id="password" style="margin-bottom:20px" align="center">
-				
-				<button type="button" id="login" class="btn btn-primary" onclick="login()"> 
-					<font size="3" face="verdana"> Sign In </font>
-				</button>
-				</center>
-			</div>	
-			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-2"></div>
-    	</div>
+    	<div class="sidenav" id="sidebar" style="display:none;">
+  			<p align="left" style="margin-top: 0px"><font size="6" color="white" face="verdana"><br/> &emsp; Jira Tool </font></p>
+  			<br/>
+  			<br/>
+  			<button type="button" id="login" class="btn btn-primary" onclick="getIssuesCurrentUser()"> 
+							<font size="3" face="verdana"> &nbsp; Get My Issues &nbsp;</font>
+			</button>
+			<br/>
+  			<br/>
+			<button type="button" id="login" class="btn btn-primary" onclick="getWorklogCurrentUser()"> 
+							<font size="3" face="verdana"> Get My Work Log </font>
+			</button>
+		</div>
+		
+		<div>
+			<table class="table" id="cardDetails" style="margin-left: 300px">
+			</table>
+		</div>
 </body>
 </html>
